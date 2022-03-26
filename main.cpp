effe6750f0b08a95c9ee3d8be34ca473ad7c863c
bhristt <cjovelserpas@nevada.unr.edu>
2022-03-25 20:29:48 -0700

    Add testing Money class operators to main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,47 @@
+// testing operators for money class part written by Christian Jovel Serpas
+// March 25 2022
+
 #include <iostream>
 #include "Money.h"
 
 int main() {
-    Money chris(100, 9);
-    Money andy(200, 10);
-    std::cout << chris << std::endl;
-    std::cout << andy << std::endl;
+    // test all operators for money class
+    Money m1(100, 99);
+    Money m2(100, 99);
+    Money m3 = m1 + m2;
+    Money m4 = m1 - m2;
+    // greater than
+    if (m1 > m2) {
+        std::cout << "m1 greater than m2" << std::endl;
+    } else {
+        std::cout << "m1 is not greater than m2" << std::endl;
+    }
+    // less than
+    if (m1 < m2) {
+        std::cout << "m1 less than m2" << std::endl;
+    } else {
+        std::cout << "m1 is not less than m2" << std::endl;
+    }
+    // equal to & not equal to
+    if (m1 == m2) {
+        std::cout << "m1 is equal to m2" << std::endl;
+    } else if (m1 != m2) {
+        std::cout << "m1 not equal to m2" << std::endl;
+    }
+    // greater than or equal to
+    if (m1 >= m3) {
+        std::cout << "m1 is greater than or equal to m3" << std::endl;
+    } else {
+        std::cout << "m1 is not greater than or equal to m3" << std::endl;
+    }
+    // less than or equal to
+    if (m1 <= m3) {
+        std::cout << "m1 is less than or equal to m3" << std::endl;
+    } else {
+        std::cout << "m1 is not less than or equal to m3" << std::endl;
+    }
+    // adding and subtracting
+    std::cout << "m3 = m1 + m2 = " << m3 << std::endl;
+    std::cout << "m4 = m1 - m2 = " << m4 << std::endl;
     return 0;
 }
