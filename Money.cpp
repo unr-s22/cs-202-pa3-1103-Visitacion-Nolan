5e918bea73d5ae10d4060ce008bd80c3358c1381
bhristt <cjovelserpas@nevada.unr.edu>
2022-03-26 00:00:26 -0700

    Add negative number compatibility

--- Money.cpp
+++ Money.cpp
@@ -6,41 +6,6 @@
 #include "Money.h"
 
 //
-// internal functions
-//
-
-int get9s(int n) {
-    int nines = 9;
-    while (nines < n) {
-        nines += nines * 10;
-    }
-    return nines;
-}
-int get10s(int n) {
-    int tens = 10;
-    while (tens < n) {
-        tens *= 10;
-    }
-    return tens;
-}
-int get10sComp(int n, int nines) {
-    int tensComp = nines - n + 1;
-    return tensComp;
-}
-int getDif(int a, int b) {
-    int b_nines = get9s(b);
-    int b_tens = get10s(b);
-    int b_tensComp = get10sComp(b, b_nines);
-    int s = a + b_tensComp;
-    if (s > b_nines) {
-        s -= b_tens;
-    } else {
-        s = get10sComp(s, b_nines);
-    }
-    return s;
-}
-
-//
 // operators
 //
 
@@ -96,8 +61,8 @@ Money Money::operator+(const Money &m) {
         }
         resultNeg = isNegative;
     } else {
-        dollars_sum = getDif(dollars, m.dollars);
-        cents_sum = getDif(cents, m.cents);
+        dollars_sum = std::abs(dollars - m.dollars);
+        cents_sum = std::abs(cents - m.cents);
         resultNeg = dollars < m.dollars || (dollars == m.dollars && cents < m.cents) ? !isNegative : isNegative;
     }
     Money result(dollars_sum, cents_sum, resultNeg);
@@ -108,8 +73,8 @@ Money Money::operator-(const Money &m) {
     bool resultNeg;
     int dollars_dif, cents_dif;
     if ((!isNegative && !m.isNegative) || (isNegative && m.isNegative)) {
-        dollars_dif = getDif(dollars, m.dollars);
-        cents_dif = getDif(cents, m.cents);
+        dollars_dif = std::abs(dollars - m.dollars);
+        cents_dif = std::abs(cents - m.cents);
         resultNeg = dollars < m.dollars || (dollars == m.dollars && cents < m.cents) ? !isNegative : isNegative;
     } else {
         dollars_dif = dollars + m.dollars;
